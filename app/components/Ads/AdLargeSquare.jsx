4abbbf23005fac3b7ed742b7477b2826fe3fb7f9
piousbox <piousbox@gmail.com>
2018-02-04 17:48:01 -0800

    and adsense

--- app/components/Ads/AdLargeSquare.jsx
+++ app/components/Ads/AdLargeSquare.jsx
@@ -4,8 +4,8 @@ import AdSense from 'react-adsense'
 class AdLargeSquare extends React.Component {
   render () {
     return (
-      <div className="adContainer" style={{ width: '336px', height: '280px', background: 'gray', color: 'white' }} >
-        <AdSense.Google client="ca-pub-5283251584689528" slot="8263704264" style={{ width: '336px', height: "280px" }} />
+      <div className="adContainer" style={{ width: '336px', height: '280px', color: 'white' }} >
+        <AdSense.Google client="ca-pub-5283251584689528" slot="8263704264" style={{ width: 336, height: 280, float: 'left' }} format='' />
       </div>
     )
   }
